fbbecb69a143f356ccaa2db13f3be460d2770c63
Tu <thanhtuhr01@gmail.com>
2024-02-07 16:35:03 +0700

    add client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,86 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { InitiateRequest } from './proto/chat_pb';
-import {ChatServiceClient} from './proto/ChatServiceClientPb';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { ChatServiceClient } from "./proto/ChatServiceClientPb";
+import {
+  StreamRequest,
+  InitiateRequest,
+  StreamMessage,
+  UserStreamResponse,
+  User,
+  MessageRequest,
+} from "./proto/chat_pb";
+import Greeting from "./components/Greeting";
+import Chat from "./components/Chat";
+
+export const client = new ChatServiceClient("http://localhost:8080");
+
+export type Session = { id: number; name: string; avatar: string };
 
 function App() {
-  useEffect(()=> {
-    (async () => {
-      const client = new ChatServiceClient("http://localhost:8080");
-      const req = new InitiateRequest();
-      req.setName('Tu');
-      req.setAvatarUrl('Tu');
-      const res = await client.chatInitiate(req, {}).catch(console.error);
-      console.log(res)
+  const [user, setUser] = useState<Session>();
+  const [messages, setMessages] = useState<Array<StreamMessage.AsObject>>([]);
+  const [userList, setUserList] = useState<Array<User.AsObject>>([]);
+
+  const handleEnterChat = (name: string, avatar: string) => {
+    const intiateReq = new InitiateRequest();
+    intiateReq.setName(name);
+    intiateReq.setAvatarUrl(avatar);
+    client.chatInitiate(intiateReq, {}, (err, resp) => {
+      if (err) throw err;
+      const id = resp.getId();
+      setUser({ id, name, avatar });
+    });
+  };
+
+  const handleSendMessage = (msg: string, onSuccess: () => void) => {
+    console.log(user, !user);
+    if (!user) return;
+    const req = new MessageRequest();
+    req.setId(user.id);
+    req.setMessage(msg);
+    console.log("here we go");
+    client.sendMessage(req, {}, (err, resp) => {
+      if (err) throw err;
+      onSuccess();
+    });
+  };
+
+  useEffect(() => {
+    if (!user) return;
+
+    const chatReq = new StreamRequest();
+    (() => {
+      chatReq.setId(user.id);
+      const chatStream = client.chatStream(chatReq);
+      chatStream.on("data", (chunk) => {
+        const msg = (chunk as StreamMessage).toObject();
+        console.log(msg);
+        setMessages((prev) => [...prev, msg]);
+      });
     })();
-  }, []);
+
+    (() => {
+      const userListStream = client.userStream(chatReq);
+      userListStream.on("data", (chunk) => {
+        const { usersList } = (chunk as UserStreamResponse).toObject();
+        console.log(usersList);
+        setUserList(usersList);
+      });
+    })();
+  }, [user]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <Chat
+          user={user}
+          userList={userList}
+          messages={messages}
+          onMessageSubmit={handleSendMessage}
+        />
+      ) : (
+        <Greeting onUsernameEnter={handleEnterChat} />
+      )}
     </div>
   );
 }
